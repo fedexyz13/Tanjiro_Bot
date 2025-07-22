2f2e606fcc9c34d97934c09de1500954373ddb81
fedexyz13 <Fedexyzbots250@gmail.com>
2025-07-22 19:09:28 -0300

    Update jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -2,7 +2,7 @@ let handler = async (m, { conn}) => {
   const jid = m.chat;
 
   try {
-    await conn.reply(jid, `🤖 *¡Hola!* Este bot no está disponible en uso de .code o .Qr\n\n📌 Si deseas integrarlo en tu propio espacio, puedes comunicarte con el creador para más información:\n\n📞 *WhatsApp:* [+54 9 11 5617 8758](https://wa.me/5491156178758)\n📧 *Correo electrónico:* fedelanyt20@gmail.com\n🌐 *Instagram:* https://www.instagram.com/fedexyz13`, m);
+    await conn.reply(jid, `🤖 *¡Hola!* Este bot no está disponible en uso de .code o .Qr\n\n📌 Si deseas integrarlo en tu grupo puedes hablar con mi creador`, m);
 } catch (error) {
     console.error('Error al enviar mensaje informativo:', error);
     conn.reply(jid, '⚠️ No se pudo enviar el mensaje. Intenta nuevamente más tarde o revisa el enlace.', m);
