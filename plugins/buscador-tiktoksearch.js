e6e10c7dac2b0093cde2467bc62f5c836c5e9151
fedexyz13 <Fedexyzbots250@gmail.com>
2025-07-22 08:47:32 -0300

    Update buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -1,60 +1,55 @@
-import axios from 'axios'
-const {proto, generateWAMessageFromContent, prepareWAMessageMedia, generateWAMessageContent, getDevice} = (await import("@whiskeysockets/baileys")).default
+import fetch from 'node-fetch';
 
-let handler = async (message, { conn, text, usedPrefix, command }) => {
-if (!text) return conn.reply(message.chat, `${emoji} Por favor, ingrese lo que desea buscar en tiktok.`, message)
-async function createVideoMessage(url) {
-const { videoMessage } = await generateWAMessageContent({ video: { url } }, { upload: conn.waUploadToServer })
-return videoMessage
-}
-async function shuffleArray(array) {
-for (let i = array.length - 1; i > 0; i--) {
-const j = Math.floor(Math.random() * (i + 1));
-[array[i], array[j]] = [array[j], array[i]]
-}
-}
+let handler = async(m, { conn, text, usedPrefix, command }) => {
+
+if (!text) return m.reply(`🍭 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Crow Edits`);
+
 try {
-await message.react(rwait)
-conn.reply(message.chat, `${emoji2} Descargando Su Video, espere un momento...`, message)
-let results = []
-let { data: response } = await axios.get('https://apis-starlights-team.koyeb.app/starlight/tiktoksearch?text=' + text)
-let searchResults = response.data
-shuffleArray(searchResults)
-let selectedResults = searchResults.splice(0, 7)
-for (let result of selectedResults) {
-results.push({
-body: proto.Message.InteractiveMessage.Body.fromObject({ text: null }),
-footer: proto.Message.InteractiveMessage.Footer.fromObject({ text: dev }),
-header: proto.Message.InteractiveMessage.Header.fromObject({
-title: '' + result.title,
-hasMediaAttachment: true,
-videoMessage: await createVideoMessage(result.nowm)
-}),
-nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.fromObject({ buttons: [] })})}
-const responseMessage = generateWAMessageFromContent(message.chat, {
-viewOnceMessage: {
-message: {
-messageContextInfo: {
-deviceListMetadata: {},
-deviceListMetadataVersion: 2
-},
-interactiveMessage: proto.Message.InteractiveMessage.fromObject({
-body: proto.Message.InteractiveMessage.Body.create({ text: `${emoji} Resultado de: ` + text }),
-footer: proto.Message.InteractiveMessage.Footer.create({ text: '⪛✰ Tiktok - Busquedas ✰⪜' }),
-header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
-carouselMessage: proto.Message.InteractiveMessage.CarouselMessage.fromObject({ cards: [...results] })})}}
-}, { quoted: message })
-await message.react(done)
-await conn.relayMessage(message.chat, responseMessage.message, { messageId: responseMessage.key.id })
-} catch (error) {
-await conn.reply(message.chat, error.toString(), message)
-}}
+let api = `https://delirius-apiofc.vercel.app/search/tiktoksearch?query=${text}`;
 
-handler.help = ['tiktoksearch <txt>']
-handler.tags = ['buscador']
-handler.command = ['tiktoksearch', 'ttss', 'tiktoks']
-handler.group = true
-handler.register = true
-handler.coin = 2
+let response = await fetch(api);
+let json = await response.json();
 
-export default handler
+m.react('🕑');
+let txt = `🔎 \`TIKTOK - SEARCH\`.`;
+      for (let i = 0; i < (5 <= json.meta.length ? 5 : json.meta.length); i++) {
+    let meta = json.meta[i];
+    txt += `\n\n`;
+    txt += `✧ *Titulo:* ${meta.title}\n`
+    txt += `✧ *Likes:* ${meta.like}\n`
+    txt += `✧ *Comentarios:* ${meta.coment}\n`
+    txt += `✧ *Compartidas:* ${meta.share}\n`
+    txt += `✧ *Link:* ${meta.url}`;
+     }
+
+m.react('🕒');
+let metaa = json.meta[0];
+conn.sendMessage(m.chat, { 
+        text: txt, 
+        footer: dev, 
+        buttons: [
+            {
+                buttonId: `${usedPrefix}tiktok ${metaa.url}`,
+                buttonText: { displayText: 'Descargar Video' }
+            },
+            {
+                buttonId: `${usedPrefix}ttmp3 ${metaa.url}`,
+                buttonText: { displayText: 'Descargar Audio' }
+            }
+        ],
+        viewOnce: true,
+        headerType: 4
+    }, { quoted: m });
+m.react('✅');
+
+} catch (e) {
+m.reply(`Error: ${e.message}`);
+m.react('✖️');
+ }
+};
+
+handler.help = ['tiktoksearch'];
+handler.tag = ['buscador'];
+handler.command = ['tiktoksearch', 'ttsearch'];
+
+export default handler;
