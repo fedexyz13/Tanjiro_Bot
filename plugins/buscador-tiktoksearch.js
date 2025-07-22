537ff327fe134bf6c846bdc43c5da2cf1c9dea99
fedexyz13 <Fedexyzbots250@gmail.com>
2025-07-22 08:49:26 -0300

    Update buscador-tiktoksearch.js

--- plugins/buscador-tiktoksearch.js
+++ plugins/buscador-tiktoksearch.js
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 
 let handler = async(m, { conn, text, usedPrefix, command }) => {
 
-if (!text) return m.reply(`🍭 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Crow Edits`);
+if (!text) return m.reply(`🌙 Ingrese Un Texto Para Buscarlo En Tiktok\n> *Ejemplo:* ${usedPrefix + command} Tanjiro Edits`);
 
 try {
 let api = `https://delirius-apiofc.vercel.app/search/tiktoksearch?query=${text}`;
