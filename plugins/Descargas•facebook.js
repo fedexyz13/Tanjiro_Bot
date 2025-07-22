6c03b02874400b926a15b860dc8d859a7a38d5c9
fedexyz13 <Fedexyzbots250@gmail.com>
2025-07-22 12:31:13 -0300

    Update Descargas•facebook.js

--- plugins/Descargas•facebook.js
+++ plugins/Descargas•facebook.js
@@ -1,47 +1,51 @@
-import { igdl } from 'ruhend-scraper'
+import { igdl } from 'ruhend-scraper';
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
-if (!args[0]) {
-return conn.reply(m.chat, '🚩 Ingresa Un Link De Facebook', m, rcanal)}
-let res
-try {
-await m.react(rwait)
-conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
-title: packname,
-body: dev,
-previewType: 0, 
-thumbnail: icons,
-sourceUrl: channel }}})
-res = await igdl(args[0])
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al obtener datos. Verifica el enlace.', m, fake)}
-let result = res.data
-if (!result || result.length === 0) {
-return conn.reply(m.chat, '🚩 No se encontraron resultados.', m, fake)}
-let data
-try {
-await m.react(rwait)
-data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al procesar los datos.', m, rcanal)}
-if (!data) {
-return conn.reply(m.chat, '🚩 No se encontró una resolución adecuada.', m, rcanal)}
-let video = data.url
-try {
-await m.react(rwait)
-await conn.sendMessage(m.chat, { video: { url: video }, caption: '🚩 *Video de Facebook*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m })
-await m.react(done)
-} catch {
-await m.react(error)
-return conn.reply(m.chat, '🚩 Error al enviar el video.', m, rcanal)}}
+  if (!args[0]) {
+    return conn.reply(m.chat, '*\`Ingresa El link Del vídeo a descargar 🤍\`*\n> De Preferencia Que Sea Un Reel 🍭', m, fake);
+  }
 
-handler.help = ['facebook', 'fb']
+  await m.react('🕒');
+  let res;
+  try {
+    res = await igdl(args[0]);
+  } catch (error) {
+    return conn.reply(m.chat, '*`Error al obtener datos. Verifica el enlace.`*', m);
+  }
+
+  let result = res.data;
+  if (!result || result.length === 0) {
+    return conn.reply(m.chat, '*`No se encontraron resultados.`*', m);
+  }
+
+  let data;
+  try {
+    data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)");
+  } catch (error) {
+    return conn.reply(m.chat, '*`Error al procesar los datos.`*', m);
+  }
+
+  if (!data) {
+    return conn.reply(m.chat, '*`No se encontró una resolución adecuada.`*', m);
+  }
+
+  await m.react('✅');
+  let video = data.url;
+// let api = await(await fetch(`https://delirius-apiofc.vercel.app/download/facebook?url=${args[0]}`)).json();
+
+// let vid = api.urls[0].hd|| api.urls[0].sd;
+
+  try {
+    await conn.sendMessage(m.chat, { video: { url: video }, caption: '《★》 *Descargado Con Exito ✓*', fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: m });
+  } catch (error) {
+    return conn.reply(m.chat, `*Error al enviar el video.*\n> ${error.message}`, m);
+  await m.react('❌');
+  }
+};
+
+handler.help = ['fb *<link>*'];
 handler.tags = ['descargas']
-handler.command = ['facebook', 'fb']
-handler.cookies = 1
-handler.register = true
+handler.command = /^(fb|facebook|fbdl)$/i;
+handler.estrellas = 5;
 
-export default handler
+export default handler;                                                                                                                                                                                                                                          
