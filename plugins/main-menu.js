ed93e3017d88e4ee67fc618554f4f3ed284743c0
fedexyz13 <Fedexyzbots250@gmail.com>
2025-07-24 11:42:41 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -99,6 +99,9 @@ let handler = async (m, { conn, usedPrefix: _p}) => {
 
     const text = _text.replace(/%(\w+)/g, (_, key) => replace[key] || '');
 
+     const imageURL = 'https://files.catbox.moe/wav09n.jpg';
+    const imgBuffer = await fetch(imageURL).then(res => res.buffer());
+    
     await conn.sendMessage(m.chat, {
       text,
       contextInfo: {
@@ -108,6 +111,7 @@ let handler = async (m, { conn, usedPrefix: _p}) => {
         forwardedNewsletterMessageInfo: {
           newsletterJid: channelRD.id,
           serverMessageId: 100,
+          thumbnail: imgBuffer,
           newsletterName: channelRD.name
 }
 }
